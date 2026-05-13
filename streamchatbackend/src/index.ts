a41279f531e5224ef762c978b840f7d4965a7ac6
Chamith <93605618+chamwithj@users.noreply.github.com>
2026-05-13 21:32:40 +0530

    first commit

---
+++ streamchatbackend/src/index.ts
@@ -0,0 +1,21 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express"
+import { apikey } from "./serverClient";
+
+const app = express()
+app.use(express.json())
+app.use(cors({origin: "*"}))
+
+app.get("/",(req,res) =>{
+  res.json({
+    message:"Ai writing assistant server is running",
+    apikey: apikey,
+
+  });
+});
+
+const port = process.env.PORT ||4000
+app.listen(port, ()=> {
+  console.log(`Server is running at http://localhost:${port}`);
+})
